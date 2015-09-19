ac85ba8d56b66f2fad95f4993202186df407db91
clareification <justclarifying@gmail.com>
2015-09-19 16:21:05 -0400

    commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var indico = require('indico.io');
 indico.apiKey = '68cd5c1dafb61295a5f288b517bd083b';
+var Firebase = require("firebase");
+var q = require('q');
 
 settings = {"api_key": "68cd5c1dafb61295a5f288b517bd083b"};
 
@@ -45,20 +47,23 @@ var gifSentiments= [];
 
 //Put cat gifs in an array
 reddit('/r/catgifs/hot').listing().then(function(slice){
-	console.log("test");
 	slice.children.forEach(function(child){
 		gifUrls.push(child.data.url);
+		displayVariables();
+
+		//Get text tags for each child
+		indico.texttags(child.data.title).then(function(res){
+		gifMeanings.push(setFeatures(res));
+		});
+
+
 		indico.sentiment(child.data.title)
   .then(function(res) {
-    gifMeanings.push(res);
-    console.log(res);
-  }).catch(function(err) {
-    console.warn(err);
-  });
-	})
-	console.log("Gif Urls: ");
-	console.log("Gifs " + gifUrls);
+    gifSentiments.push(res);
+   // console.log(res);
+  })
 		});
+	
 
 
 //Put news articles in Array
@@ -77,61 +82,63 @@ var newsIndex = 0;
 
 //Get news data from reddit
 reddit('/r/news/hot').listing().then(function(slice) {
-
   slice.children.forEach(function(child){
+
+  	//Add url to links array
   	newsLinks.push(child.data.url);
+
+  	//Add the topics of title to topic array
   	var currTitle = child.data.title;
   	indico.texttags(currTitle, settings)
   .then(function(res) {
-  	var currArray = [];
-  	currArray.setFeatures(res);
+  	var currArray = setFeatures(res); 	
     newsTopics.push(currArray);
-    console.log("News Topics \n" + newsTopics);
-  }).catch(function(err) {
-    console.warn(err);
-  })
-  .then(function() {
+  });
   	indico.sentiment(currTitle).then(function(res){
   		newsSentiments.push(res);
-  		console.log("Sentiments \n" + newsSentiments);
   	});
-  	}
-  );
-
   });
-  
-  // Get a promise for the next slice in this 
-  // listing (the next page!)
-
-  
 });
+
+
+console.log("difference : " + (newsSentiments[1] - gifSentiments[1]));
 
 //More Functions
 
-function sumFunction(a, b){
-	var c = [];
+function sumSquares(a , b){
+	var sum = 0;;
 	for(var i = 0; i<a.length && i<b.length; i++){
-		c[i] = Math.pow(a[i] - b[i], 2);
+		sum += Math.pow(a[i] - b[i], 2);
 	}
-	var sum = 0;
-	for(var j = 0; j<c.length; j++){
-		sum += c[i];
-	}
+
+	//for debugging
+	console.log("Sum is " + sum);
 	return sum;
 }
 
-  function setFeatures(str) {
-  	var features = [];
-  		indico.texttags(str, settings)
-  		.then(function(res) {
-  				for(var key in res){
-  					if(res.hasOwnProperty(key)){
-  						features.push(res[key]);
-  					}
-  				}
-  		})
-  		return features;
+ function setFeatures(res) {
+  	var features = []
+		for(var key in res){
+			if(res.hasOwnProperty(key)){
+				//console.log(res[key]);
+				features.push(res[key]);
+				}
+			}
+			//console.log(features);
+	return features;
   }
 
 
+function displayVariables(){
+	console.log("gm : " + gifMeanings.length + " gs " + gifSentiments.length + " ns" + newsSentiments.length + "nt" + newsTopics.length);
+}
 
+//Save to firebase
+
+var ref = new Firebase("https://giphyindex.firebaseio.com/");
+
+var contentRef = ref.child("content");
+
+contentRef.set({
+	gif_url: ""
+})
